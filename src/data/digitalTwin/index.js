598a9d8cc5097f1e383cb70ed3eab0aa43b2aca3
Andres <andres.zapata@headai.com>
2025-09-17 10:21:45 +0300

    HotFix: Making available DigitalTwin class in the library interface

--- src/data/digitalTwin/index.js
+++ src/data/digitalTwin/index.js
@@ -1,9 +1,11 @@
+import DigitalTwin from "./DigitalTwin";
 import DigitalTwinProcessing from "./DigitalTwinProcessing";
 import GraphProcessor from "./GraphProcessor";
 
 import colors from "./colors";
 
 const digitalTwin = {
+    DigitalTwin,
     DigitalTwinProcessing,
     GraphProcessor,  
     colors,
